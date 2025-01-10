53067af703d35cc8f875283c237d106b62c20b8c
Le Ba Vui <lebavui@gmail.com>
2025-01-10 23:42:52 +0700

    Update

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
-Deno.serve((req: Request) => new Response("Bây giờ là: " + Date.now()));
+import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
+
+const app = new Hono();
+const kv = await Deno.openKv();
+
+// Redirect root URL
+app.get("/", (c) => c.redirect("/books"));
+
+// List all books
+app.get("/books", async (c) => {
+  const iter = await kv.list({ prefix: ["books"] });
+  const books = [];
+  for await (const res of iter) books.push(res);
+
+  return c.json(books);
+});
+
+// Create a book (POST body is JSON)
+app.post("/books", async (c) => {
+  const body = await c.req.json();
+  const result = await kv.set(["books", body.title], body);
+  return c.json(result);
+});
+
+// Get a book by title
+app.get("/books/:title", async (c) => {
+  const title = c.req.param("title");
+  const result = await kv.get(["books", title]);
+  return c.json(result);
+});
+
+// Delete a book by title
+app.delete("/books/:title", async (c) => {
+  const title = c.req.param("title");
+  await kv.delete(["books", title]);
+  return c.text("");
+});
+
+Deno.serve(app.fetch);
